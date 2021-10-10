edf30cdd5c8db14e0bf7dea49aee4a81db305283
pawor <pawor.emi@gmail.com>
2021-10-10 22:05:22 +0700

    create Store.js

--- pages/_document.js
+++ pages/_document.js
@@ -7,11 +7,12 @@ export default class MyDocument extends Document{
         return(
             <Html lang="en">
                 <Head>
-                    <body>
-                        <Main />
-                        <NextScript />
-                    </body>
+                    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"/>
                 </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
             </Html>
         )
     }
