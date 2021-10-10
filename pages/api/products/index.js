b18a88c2560b6f756494fa02ec77515217bd6aa0
pawor <pawor.emi@gmail.com>
2021-10-11 00:28:08 +0700

    create product API

--- pages/api/products/index.js
+++ pages/api/products/index.js
@@ -7,5 +7,8 @@ const handler = nc();
 handler.get(async(req,res) => {
     await db.connect();
     const products = await Product.find({});
+    await db.disconnect();
     res.send(products);
-})
+});
+
+export default handler;
